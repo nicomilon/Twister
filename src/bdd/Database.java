6c84e9a2af4214734e073cad11c4a519eaa1a47e
Edgar <munroedgar@hotmail.com>
2018-02-27 18:01:58 +0100

    Fix : Changed Database file (no more DBStatic class, all in same file)
    Added directory misc with sql queries for each DB, and JSON file with Postman collection of server testing queries

--- src/bdd/Database.java
+++ src/bdd/Database.java
@@ -7,40 +7,58 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class Database {
-	private DataSource dataSource;
-	private static Database database;
+    public enum DBType {MYSQLDB, SQLSERVER}
 
-	public Database(String jndiname) throws SQLException {
-		try {
-			dataSource = (DataSource) new InitialContext().lookup("java:comp/env/" + jndiname);
-		} catch (NamingException e) {
-			// Handle error that it’s not configured in JNDI.
-			throw new SQLException(jndiname + " is missing in JNDI! : " + e.getMessage());
-		}
-	}
+    private static boolean mysql_pooling;
+    private static final String mysql_username = "root";
+    private static final String mysql_password = "toor";
+    private static final String mysql_host = "localhost:3306";
+    private static final String mysql_db = "twista";
 
-	public Connection getConnection() throws SQLException {
-		return dataSource.getConnection();
-	}
+    private static final String sqlserver_CS = "jdbc:sqlserver://localhost\\EL-GRINGO:1433;database=Twister";
 
-	
-	public static Connection getMySQLConnection() throws SQLException {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		if (DBStatic.mysql_pooling == false) {
-			return (DriverManager.getConnection("jdbc:mysql://" + DBStatic.mysql_host + "/" + DBStatic.mysql_db,
-					DBStatic.mysql_username, DBStatic.mysql_password));
-		} else {
-			if (database == null) {
-				database = new Database("jdbc/db");
-			}
-			return (database.getConnection());
-		}
-	}
+
+    private DataSource dataSource;
+    private static Database database;
+
+    public Database(String jndiname) throws SQLException {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/" + jndiname);
+        } catch (NamingException e) {
+            // Handle error that it’s not configured in JNDI.
+            throw new SQLException(jndiname + " is missing in JNDI! : " + e.getMessage());
+        }
+    }
+
+    public Connection getConnection(DBType type) throws SQLException {
+        switch (type) {
+            case MYSQLDB:
+                return dataSource.getConnection();
+            case SQLSERVER:
+                return DriverManager.getConnection(sqlserver_CS);
+            default:
+                return null;
+        }
+
+    }
+
+
+    public static Connection getMySQLConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (mysql_pooling == false) {
+            return (DriverManager.getConnection("jdbc:mysql://" + mysql_host + "/" + mysql_db,
+                    mysql_username, mysql_password));
+        } else {
+            if (database == null) {
+                database = new Database("jdbc/db");
+            }
+            return (database.getConnection(DBType.MYSQLDB));
+        }
+    }
 }
 
 
